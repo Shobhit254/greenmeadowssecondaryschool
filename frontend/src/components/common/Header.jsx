7a6260541db04f106fb6fbaf8a28479f9191f7c1
Shobhit Anand <kumarshobhit254@gmail.com>
2026-03-01 14:09:44 +0530

    Updated site ID

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -100,7 +100,7 @@
 import React from 'react'
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-// import Container from 'react-bootstrap/Container';
+import Container from 'react-bootstrap/Container';
 import logo from '../../assets/Images/logo.png'
 
 const Header = () => {
